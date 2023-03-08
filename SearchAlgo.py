176b7d7cbe9705ad5dc831ba1405614afa1b5fab
Hamza Gabajiwala <hamzajg16@gmail.com>
2023-03-08 00:04:51 +0000

    Working A* code

--- SearchAlgo.py
+++ SearchAlgo.py
@@ -11,36 +11,70 @@ def calculateManhattanDistance(x, y=(1, 1)):
 
 class SearchAlgo:
     def __init__(self, m=None, goal=None, algo=None):
+
         self.m = m
         if goal is None:
             raise AssertionError("Goal Cannot be None")
-        if type(goal) != type({}):
+        if type(goal) is not type({}):
             raise AssertionError(f"Goal of Incorrect Type:\n\tExpected: {type({})}\n\tGot: {type(goal)}")
         self.goal = goal
         self.algo = algo
 
         self.nodes = NODES
 
-        self.start = (self.m.rows, self.m.cols)
+        # | DFS BFS PARAMETERS
         self.explored = []
         self.frontier = []
-        self.currCell = None
-        self.searchedPath = {}
 
+        # | A* PARAMETERS
+        self.g_score = {}
+        self.f_score = {}
+        self.store = PriorityQueue()
+
+        self.currCell = None
+        self.start = (self.m.rows, self.m.cols)
+
+        # | PATHS
+        self.algoPath = {}
+        self.searchedPath = []
         self.forward_path = {}
-        self.cell = (goal['x'], goal['y'])
+        self.forwardPathCell = (goal['x'], goal['y'])
 
     def set_params(self):
-        self.explored = [self.start]
-        self.frontier = [self.start]
+        if self.algo in ['dfs', 'bfs']:
+            self.explored = [self.start]
+            self.frontier = [self.start]
 
-    def set_algorithm(self):
+        elif self.algo in 'a*':
+            self.searchedPath = [self.start]
+
+            self.g_score = {cell: float('inf') for cell in self.m.grid}
+            self.f_score = {cell: float('inf') for cell in self.m.grid}
+
+            self.g_score[self.start] = 0
+            self.f_score[self.start] = calculateManhattanDistance(self.start)
+
+            self.store.put((self.f_score, self.f_score, self.start))
+
+        else:
+            raise AssertionError("Algorithm specified incorrectly")
+
+    def set_searched_path(self):
+        self.searchedPath.append(self.currCell)
+
+    def get_stopping_condition(self):
+        if self.algo in ['dfs', 'bfs']:
+            return len(self.frontier) > 0
+        elif self.algo == 'a*':
+            return not self.store.empty()
+
+    def get_current_cell(self):
         if self.algo == 'dfs':
             return self.frontier.pop()
         elif self.algo == 'bfs':
             return self.frontier.pop(0)
-        else:
-            raise AssertionError("Algorithm Specified Incorrectly")
+        elif self.algo == 'a*':
+            return self.store.get()[2]
 
     def move(self, direction=None):
         if direction is None:
@@ -56,39 +90,51 @@ class SearchAlgo:
                 return self.currCell[0], self.currCell[1] - 1
 
     def search_path(self):
-        while len(self.frontier) > 0:
-            self.currCell = self.set_algorithm()
+        while self.get_stopping_condition():
+            self.currCell = self.get_current_cell()
+
+            if self.algo in ['dfs', 'a*']:
+                self.set_searched_path()
+
             if self.currCell == (self.goal['x'], self.goal['y']):
                 break
 
             for d in self.nodes:
                 if self.m.maze_map[self.currCell][d]:
-                    """
-                    self.nodes
-                        E = 0
-                        S = 1
-                        N = 2
-                        W = 3
-                    """
                     if d == self.nodes[0]:
-                        childCell = self.move(direction=self.nodes[0])
+                        child = self.move(direction=self.nodes[0])
                     elif d == self.nodes[1]:
-                        childCell = self.move(direction=self.nodes[1])
+                        child = self.move(direction=self.nodes[1])
                     elif d == self.nodes[2]:
-                        childCell = self.move(direction=self.nodes[2])
+                        child = self.move(direction=self.nodes[2])
                     elif d == self.nodes[3]:
-                        childCell = self.move(direction=self.nodes[3])
+                        child = self.move(direction=self.nodes[3])
 
-                    if childCell in self.explored:
-                        continue
-                    self.explored.append(childCell)
-                    self.frontier.append(childCell)
-                    self.searchedPath[childCell] = self.currCell
+                    # self.algorithm_logic(child)
+
+                    if self.algo in ['dfs', 'bfs']:
+                        if child in self.explored:
+                            continue
+                        self.explored.append(child)
+                        self.frontier.append(child)
+                        self.algoPath[child] = self.currCell
+
+                        if self.algo == 'bfs':
+                            self.set_searched_path()
+                    elif self.algo == 'a*':
+                        temp_g_score = self.g_score[self.currCell] + 1
+                        temp_f_score = temp_g_score + calculateManhattanDistance(child)
+
+                        if temp_f_score < self.f_score[child]:
+                            self.g_score[child] = temp_g_score
+                            self.f_score[child] = temp_f_score
+                            self.store.put((temp_f_score, calculateManhattanDistance(child), child))
+                            self.algoPath[child] = self.currCell
 
     def get_forward_path(self):
-        while self.cell != self.start:
-            self.forward_path[self.searchedPath[self.cell]] = self.cell
-            self.cell = self.searchedPath[self.cell]
+        while self.forwardPathCell != self.start:
+            self.forward_path[self.algoPath[self.forwardPathCell]] = self.forwardPathCell
+            self.forwardPathCell = self.algoPath[self.forwardPathCell]
         return self.forward_path
 
 
@@ -100,3 +146,8 @@ class DFS(SearchAlgo):
 class BFS(SearchAlgo):
     def __init__(self, m, goal):
         super().__init__(m, goal, algo='bfs')
+
+
+class AStar(SearchAlgo):
+    def __init__(self, m, goal):
+        super().__init__(m, goal, algo='a*')
