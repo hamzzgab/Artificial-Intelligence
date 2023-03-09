e34c921a0016c09266ca6aba3ec5ce168dde1c7e
Hamza Gabajiwala <hamzajg16@gmail.com>
2023-03-09 02:26:15 +0000

    Fixed colour issue on gui

--- gui.py
+++ gui.py
@@ -32,7 +32,8 @@ window.title('CS7IS2 - Assignment - 1')
 window.geometry("380x380+0+0")
 window.minsize(width=380, height=380)
 window.maxsize(width=380, height=380)
-window.config(pady=5)
+window.config(pady=5, bg='#252527')
+
 
 # ----------------FRAME-----------------------
 window.columnconfigure(0, weight=1, minsize=170)
@@ -41,31 +42,31 @@ window.rowconfigure(0, weight=1, minsize=200)
 rows, cols = (25, 25)
 goal_x, goal_y = (1, 1)
 
-frm_maze = Frame(master=window, relief=RIDGE, padx=5, pady=5, highlightbackground="#03cafc", highlightthickness=2)
+frm_maze = Frame(master=window, relief=RIDGE, padx=5, pady=5, highlightbackground="#03cafc", highlightthickness=2, bg='#252527')
 
-lbl_maze = Label(master=frm_maze, text='Maze Parameters', font='Helvetica 18 bold', foreground='#03cafc')
+lbl_maze = Label(master=frm_maze, text='Maze Parameters', font='Helvetica 18 bold', foreground='#03cafc', bg='#252527')
 
-lbl_rows = Label(master=frm_maze, text="Rows")
-lbl_cols = Label(master=frm_maze, text="Cols")
+lbl_rows = Label(master=frm_maze, text="Rows", bg='#252527', fg='white')
+lbl_cols = Label(master=frm_maze, text="Cols", bg='#252527', fg='white')
 
-ent_rows = Entry(master=frm_maze, width=5, highlightthickness=1)
+ent_rows = Entry(master=frm_maze, width=5, highlightthickness=1, highlightcolor='#03cafc', bg='#252527', fg='white')
 ent_rows.insert(END, string=f'{rows}')
-ent_cols = Entry(master=frm_maze, width=5, highlightthickness=1)
+ent_cols = Entry(master=frm_maze, width=5, highlightthickness=1, highlightcolor='#03cafc', bg='#252527', fg='white')
 ent_cols.insert(END, string=f'{cols}')
 
-lbl_goal = Label(master=frm_maze, text="Goal", font='Helvetica 14 bold', foreground='#03cafc')
+lbl_goal = Label(master=frm_maze, text="Goal", font='Helvetica 14 bold', foreground='#03cafc', bg='#252527')
 
-lbl_goalX = Label(master=frm_maze, text="X")
-lbl_goalY = Label(master=frm_maze, text="Y")
+lbl_goalX = Label(master=frm_maze, text="X", bg='#252527', fg='white')
+lbl_goalY = Label(master=frm_maze, text="Y", bg='#252527', fg='white')
 
-ent_goalX = Entry(master=frm_maze, width=5, highlightthickness=1)
+ent_goalX = Entry(master=frm_maze, width=5, highlightthickness=1, highlightcolor='#03cafc', bg='#252527', fg='white')
 ent_goalX.insert(END, string=f'{goal_x}')
-ent_goalY = Entry(master=frm_maze, width=5, highlightthickness=1)
+ent_goalY = Entry(master=frm_maze, width=5, highlightthickness=1, highlightcolor='#03cafc', bg='#252527', fg='white')
 ent_goalY.insert(END, string=f'{goal_y}')
 
 # | DRAW
 
-lbl_maze.grid(row=0, column=0, columnspan=2)
+lbl_maze.grid(row=0, column=0, columnspan=2, sticky='nw')
 
 #   | ROW COL
 lbl_rows.grid(row=1, column=0, sticky=W, pady=2)
@@ -90,19 +91,22 @@ frm_maze.grid(column=0, row=0, sticky='nsew', padx=5)
 window.columnconfigure(1, weight=1, minsize=170)
 window.rowconfigure(0, weight=1, minsize=200)
 
-frm_algo = Frame(master=window, relief=RAISED,padx=5, pady=5, highlightbackground="#ecfc03", highlightthickness=2)
+frm_algo = Frame(master=window, relief=RAISED,padx=5, pady=5, highlightbackground="#ecfc03", highlightthickness=2,
+                 bg='#252527')
 
-lbl_algo = Label(master=frm_algo, text='Search Algorithm', font='Helvetica 18 bold', foreground='#ecfc03')
-lbl_algos = Label(master=frm_algo, text='Run Algos', font='Helvetica 14 bold', foreground='#ecfc03')
+lbl_algo = Label(master=frm_algo, text='Search Algorithm', font='Helvetica 18 bold', foreground='#ecfc03', bg='#252527')
+lbl_algos = Label(master=frm_algo, text='Run Algos', font='Helvetica 14 bold', foreground='#ecfc03', bg='#252527')
 
 AlgoRunning = StringVar(value="Running Algos:\nDFS, BFS")
-lbl_algosRunning = Label(master=frm_algo, textvariable=AlgoRunning, font='Helvetica 14', foreground='#ecfc03', height=8)
+lbl_algosRunning = Label(master=frm_algo, textvariable=AlgoRunning, font='Helvetica 14', foreground='#ecfc03',
+                         height=8, bg='#252527')
 
 CheckRunMode = IntVar(value=1)
-chk_runMode = Checkbutton(master=frm_algo, text='Run Together', variable=CheckRunMode, onvalue=1, offvalue=0, command=check_changed)
+chk_runMode = Checkbutton(master=frm_algo, text='Run Together', variable=CheckRunMode, onvalue=1, offvalue=0,
+                          command=check_changed, bg='#252527', fg='white')
 
 
-listbox = Listbox(master=frm_algo, height=3, selectmode='multiple', selectforeground='white', selectbackground='green',
+listbox = Listbox(master=frm_algo, height=3, selectmode='multiple', selectforeground='#252527', selectbackground='#a7faa9',
                   activestyle='none')
 algos = ['DFS', 'BFS', 'AStar']
 
@@ -114,7 +118,7 @@ listbox.bind('<<ListboxSelect>>', listbox_used)
 
 
 # | DRAW
-lbl_algo.grid(column=0, row=0)
+lbl_algo.grid(column=0, row=0, sticky='nw')
 
 chk_runMode.grid(column=0, row=2, sticky='nw')
 
@@ -130,16 +134,16 @@ frm_algo.grid(column=1, row=0, sticky='nsew', padx=5)
 window.columnconfigure(0, weight=1, minsize=170)
 window.rowconfigure(1, weight=1, minsize=10)
 
-frm_mdp = Frame(master=window, relief=RAISED, padx=5, pady=5, highlightbackground="#ff0000", highlightthickness=2)
+frm_mdp = Frame(master=window, relief=RAISED, padx=5, pady=5, highlightbackground="#ff0000", highlightthickness=2, bg='#252527')
 
-lbl_mdp = Label(master=frm_mdp, text='Markov Decision Process', font='Helvetica 18 bold', foreground='#ff0000')
+lbl_mdp = Label(master=frm_mdp, text='Markov Decision Process', font='Helvetica 18 bold', foreground='#ff0000', bg='#252527')
 
 lbl_mdp.grid(column=0, row=0)
 
 frm_mdp.grid(column=0, row=1, sticky='nsew', columnspan=2, padx=5, pady=4)
 
 # -------------------RUN-----------------------
-btn_run = Button(text='RUN', fg='green', command=set_values)
+btn_run = Button(text='RUN', fg='green', command=set_values, bg='#252527', highlightbackground='#252527')
 btn_run.grid(column=0, row=2, sticky='nsew', columnspan=2)
 
 window.mainloop()
